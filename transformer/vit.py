4234a2911228656ce2dd9255c4c3b7c21b103f98
Pascal Bustamante <pascalbustamante46@gmail.com>
2023-10-12 16:28:34 -0500

    vit

--- transformer/vit.py
+++ transformer/vit.py
@@ -1,53 +1,50 @@
-import numpy as np
+import torch
+from torch import nn
 
 
-def create_patches(image, patch_size):
-    h, w, c = image.shape
-    image = np.reshape(image, (h // patch_size, patch_size, w // patch_size, patch_size, c))
-    image = np.transpose(image, (0, 2, 1, 3, 4))
-    patches = np.reshape(image, (-1, patch_size, patch_size, c))
-    return patches
+learning_rate = 1e-4
+num_classes = 10
+patch_size = 4
+img_size = 28
+in_channels = 1
+dropout = 0.001
+hidden_dim = 768
+adam_weight_decay = 0
+adam_betas = (0.9, 0.999)
+activaition = "gelu"
+num_encoder = 4
+embed_dim = (patch_size ** 2) * in_channels
+num_patches = (img_size // patch_size) ** 2 
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
-def create_positional_encoding(length, d_model):
-    pos = np.arange(length)[:, np.newaxis]
-    div_term = np.exp(np.arange(0, d_model, 2) * -(np.log(10000.0) / d_model))
-    positional_encoding = np.zeros((length, d_model))
-    positional_encoding[:, 0::2] = np.sin(pos * div_term)
-    positional_encoding[:, 1::2] = np.cos(pos * div_term)
-    return positional_encoding
+class PatchEmbedding(nn.Module):
+    def __init__(self, embed_dim, patch_size, num_patches, dropout, in_channels):
+        super().__init__()
+        self.patcher = nn.Sequential(
+            nn.Conv2d(
+                in_channels=in_channels,
+                out_channels=embed_dim,
+                kernel_size=patch_size,
+                stride=patch_size
+            ),
+            nn.Flatten(2)
+        )
+        self.cls_token = nn.Parameter(torch.randn(size=(1, in_channels, embed_dim)), requires_grad=True)
+        self.positoin_embeddings = nn.Parameter(torch.randn(size=(1, num_patches+1, embed_dim)), requires_grad=True)
+        self.dropout = nn.Dropout(p=dropout)
 
 
-def softmax(x):
-    e_x = np.exp(x - np.max(x))
-    return e_x / e_x.sum(axis=-1, keepdims=True)
+    def forward(self, x):
+        cls_token = self.cls_token.expand(x.shape[0], -1, -1)
 
+        x = self.patcher(x).permute(0,2,1)
+        x = torch.cat([cls_token, x], dim=1)
+        x = self.dropout(x)
+        return x
+    
 
-def multi_head_self_attention(q, k, v, num_heads):
-    d_model = q.shape[-1]
-    assert d_model % num_heads == 0
-    depth = d_model // num_heads
-    q = np.reshape(q, (q.shape[0], q.shape[1], num_heads, depth))
-    k = np.reshape(k, (k.shape[0], k.shape[1], num_heads, depth))
-    v = np.reshape(v, (v.shape[0], v.shape[1], num_heads, depth))
-    scores = np.matmul(q, k.transpose((0, 1, 3, 2))) / np.sqrt(depth)
-    attention_weights = softmax(scores)
-    output = np.matmul(attention_weights, v)
-    output = np.reshape(output, (output.shape[0], output.shape[1], d_model))
-    return output
-
-
-def feed_forward_network(x, dff):
-    return np.dot(x, dff)
-
-
-def layer_norm(x):
-    return (x - np.mean(x)) / np.std(x)
-
-
-def transformer_encoder(x, num_heads, dff):
-    attention_output = multi_head_self_attention(x, x, x, num_heads)
-    attention_output = layer_norm(attention_output + x)
-    ffn_output = feed_forward_network(attention_output, dff)
-    ffn_output = layer_norm(ffn_output + attention_output)
-    return ffn_output
+model = PatchEmbedding(embed_dim, patch_size, num_patches, dropout, in_channels).to(device)
+x = torch.randn(512, 1, 28, 28)
+print(model(x).shape)
